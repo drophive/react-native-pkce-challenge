40c21b8557dcc0611cdf594e5c00d5ce80edacd6
David Angulo <hello@davidangulo.xyz>
2020-10-09 23:39:46 +0800

    Commit

---
+++ windows/RandomBytes/RandomBytesModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using Windows.Security.Credentials.UI;
+
+using Microsoft.ReactNative.Managed;
+using Microsoft.ReactNative;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+using Windows.Devices.Power;
+using Windows.ApplicationModel;
+using Windows.System;
+
+namespace RandomBytes
+{
+    [ReactModule("RandomBytes")]
+    class RandomBytesModule
+    {
+        [ReactMethod]
+        public async void randomBytes(IReactPromise<string> promise)
+        {
+            promise.Resolve("TEST");
+        }
+    }
+}
