789c4a11a372a717beeb97384b47fab7615c2415
David Angulo <hello@davidangulo.xyz>
2020-10-09 22:43:32 +0800

    Commit

---
+++ windows/RandomBytes/ReactPackageProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ReactNative;
+using Microsoft.ReactNative.Managed;
+
+namespace RandomBytes
+{
+    public partial class ReactPackageProvider : IReactPackageProvider
+    {
+        public void CreatePackage(IReactPackageBuilder packageBuilder)
+        {
+            CreatePackageImplementation(packageBuilder);
+        }
+        partial void CreatePackageImplementation(IReactPackageBuilder packageBuilder);
+    }
+}
